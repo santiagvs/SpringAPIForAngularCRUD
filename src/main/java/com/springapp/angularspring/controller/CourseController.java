23a199ee2ff8364a006aaea246a2288b2266b68d
Santiago <victor.lacsilva0102@gmail.com>
2024-01-29 18:51:10 -0300

    feat: anotação ResponseStatus adicionada à action de criação de curso para retornar código 201

--- src/main/java/com/springapp/angularspring/controller/CourseController.java
+++ src/main/java/com/springapp/angularspring/controller/CourseController.java
@@ -2,6 +2,8 @@ package com.springapp.angularspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import com.springapp.angularspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -26,6 +29,7 @@ public class CourseController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Course create(@RequestBody Course course) {
     return courseRepository.save(course);
   }
